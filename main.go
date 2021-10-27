397e2f4d5396cd55ff682aa297978c9a85dc813c
Butin Artuom <a.butin@andersenlab.com>
2021-10-27 12:02:47 +0300

    fix

--- main.go
+++ main.go
@@ -20,21 +20,27 @@ type userPost struct {
 
 func main() {
 	logger.SetFormatter(&log.JSONFormatter{})
+	start := time.Now()
 	users := make([]User, 0)
 	posts, userPosts := make([]Post, 0), make([]Post, 0)
 	result := make([]userPost, 0)
 	var wg sync.WaitGroup
-
+	client := &http.Client{
+		Transport: &http.Transport{
+			MaxIdleConnsPerHost: 20,
+		},
+		Timeout: 10 * time.Second,
+	}
 	wg.Add(2)
 	go func() {
 		defer wg.Done()
-		if err := getResponse(userUrl, &users); err != nil {
+		if err := sendRequest(*client, userUrl, &users); err != nil {
 			panic(err)
 		}
 	}()
 	go func() {
 		defer wg.Done()
-		if err := getResponse(postUrl, &posts); err != nil {
+		if err := sendRequest(*client, postUrl, &posts); err != nil {
 			panic(err)
 		}
 	}()
@@ -43,9 +49,8 @@ func main() {
 		if err := recover(); err != nil {
 			logger.WithFields(log.Fields{
 				"package": "main",
-				"function": "getResponse",
-				"err": err,
-			}).Fatal("panic occurred:", err)
+				"err":     err,
+			}).Panic("panic occurred:", err)
 		}
 	}()
 	for _, user := range users {
@@ -57,16 +62,17 @@ func main() {
 		result = append(result, userPost{User: user, Post: userPosts})
 		userPosts = []Post{}
 	}
+	duration := time.Since(start)
+	logger.SetFormatter(&log.TextFormatter{})
+	logger.WithFields(log.Fields{
+		"package":  "main",
+		"function": "main",
+		"data":     duration,
+	}).Info("execution time ", duration)
 }
 
-func getResponse(uri string, T interface{}) error {
-	client := &http.Client{
-		Transport: &http.Transport{
-			MaxIdleConnsPerHost: 20,
-		},
-		Timeout: 10 * time.Second,
-	}
-	r, err := client.Get(baseUrl + uri)
+func sendRequest(c http.Client, uri string, T interface{}) error {
+	r, err := c.Get(baseUrl+uri)
 	if err != nil {
 		return err
 	}
@@ -74,9 +80,9 @@ func getResponse(uri string, T interface{}) error {
 		err := r.Body.Close()
 		if err != nil {
 			logger.WithFields(log.Fields{
-				"package": "main",
+				"package":  "main",
 				"function": "Body.Close",
-				"err": err,
+				"err":      err,
 			}).Error(err)
 		}
 	}()
